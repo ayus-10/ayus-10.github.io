7f0379e419765ac9c7891be52454f2cdc2b47916
ayus-10 <136055230+ayus-10@users.noreply.github.com>
2025-06-08 12:38:39 +0545

    remove full picture display, its stupid

--- index.js
+++ index.js
@@ -1,37 +1,5 @@
 const goBack = () => window.history.back();
 
-const profileImage = document.querySelectorAll(".profile-image")[0];
-const bannerImage = document.querySelectorAll(".banner-image")[0];
-const fullBannerImage = document.querySelectorAll(".full-banner-image")[0];
-const fullProfileImage = document.querySelectorAll(".full-profile-image")[0];
-
-profileImage.addEventListener("click", () =>
-  fullProfileImage.classList.toggle("visible"),
-);
-
-bannerImage.addEventListener("click", () =>
-  fullBannerImage.classList.toggle("visible"),
-);
-
-fullBannerImage.addEventListener("click", (e) => {
-  if (e.target.tagName === "ASIDE") {
-    fullBannerImage.classList.remove("visible");
-  }
-});
-
-fullProfileImage.addEventListener("click", (e) => {
-  if (e.target.tagName === "ASIDE") {
-    fullProfileImage.classList.remove("visible");
-  }
-});
-
-document.addEventListener("keydown", (event) => {
-  if (event.key === "Escape") {
-    fullProfileImage.classList.remove("visible");
-    fullBannerImage.classList.remove("visible");
-  }
-});
-
 const aboutTab = document.getElementById("tab-about");
 const experienceTab = document.getElementById("tab-experience");
 const contactTab = document.getElementById("tab-contact");
